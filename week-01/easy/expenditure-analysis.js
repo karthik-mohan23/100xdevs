23812016e98c38d2c4fd6a054f8cd467dc520baa
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-12-22 22:49:25 +0530

    completed all easy assignments from week 1

--- week-01/easy/expenditure-analysis.js
+++ week-01/easy/expenditure-analysis.js
@@ -13,65 +13,64 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-const transactions = [
-  {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: "Food",
-    itemName: "Pizza",
-  },
-  {
-    id: 2,
-    timestamp: 1656259600000,
-    price: 20,
-    category: "Food",
-    itemName: "Burger",
-  },
-  {
-    id: 3,
-    timestamp: 1656019200000,
-    price: 15,
-    category: "Clothing",
-    itemName: "T-Shirt",
-  },
-  {
-    id: 4,
-    timestamp: 1656364800000,
-    price: 30,
-    category: "Electronics",
-    itemName: "Headphones",
-  },
-  {
-    id: 5,
-    timestamp: 1656105600000,
-    price: 25,
-    category: "Clothing",
-    itemName: "Jeans",
-  },
-];
+// const transactions = [
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: "Food",
+//     itemName: "Pizza",
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1656259600000,
+//     price: 20,
+//     category: "Food",
+//     itemName: "Burger",
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1656019200000,
+//     price: 15,
+//     category: "Clothing",
+//     itemName: "T-Shirt",
+//   },
+//   {
+//     id: 4,
+//     timestamp: 1656364800000,
+//     price: 30,
+//     category: "Electronics",
+//     itemName: "Headphones",
+//   },
+//   {
+//     id: 5,
+//     timestamp: 1656105600000,
+//     price: 25,
+//     category: "Clothing",
+//     itemName: "Jeans",
+//   },
+// ];
 
 function calculateTotalSpentByCategory(transactions) {
-  let categorySet = new Set();
-
   let newArr = [];
+  let newObj = {};
 
   for (let i = 0; i < transactions.length; i++) {
-    if (categorySet.has(transactions[i])) {
-      newObj.totalSpent += transactions[i].price;
+    let item = transactions[i];
+    if (newObj.hasOwnProperty(item.category)) {
+      newObj[item.category] = newObj[item.category] + item.price;
     } else {
-      categorySet.add(transactions[i].category);
-
-      newArr.push({
-        transactions[i].category : transactions[i].category,
-        newObj["totalSpent"] : transactions[i].price
-      })
-    
+      newObj[item.category] = item.price;
     }
   }
-  return newObj;
+
+  // 2nd problem to get them into array
+  for (let key in newObj) {
+    newArr.push({ category: key, totalSpent: newObj[key] });
+  }
+  return newArr;
 }
 
-console.log(calculateTotalSpentByCategory(transactions));
+// console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
